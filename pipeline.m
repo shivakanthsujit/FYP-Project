02ec3057095d5516dd13bdcfcb008a587f3cab62
Shiva Kanth Sujit <shivakanth.sujit@gmail.com>
2021-02-22 12:51:51 +0530

    Use unified rule base

--- pipeline.m
+++ pipeline.m
@@ -4,11 +4,20 @@ global parametersq
 E = fuzzify(E, 0);
 dy = fuzzify(dy, 1);
 
-w_hat1 = parametric([E(1) dy(2)], 1, 1);
-w_hat2 = parametric([E(2) dy(3)], 1, 1);
-w_hat3 = parametric([E(2) dy(2)], 1, 1);
-w_hat4 = parametric([E(2) dy(1)], 1, 1);
-w_hat5 = parametric([E(3) dy(2)], 1, 1);
+% Step 3 + 4
+global w_hat_rules;
+w_hat_rules = {
+    [1 2],
+    [2 3],
+    [2 2],
+    [2 1],
+    [3 2]
+};
+
+w_hats = zeros(length(w_hat_rules), 1);
+for i=1:length(w_hat_rules)
+w_hats(i) = parametric([E(w_hat_rules{i}(1)) dy(w_hat_rules{i}(2))], 1, 1);
+end
 
 
 w_sum = w_hat1 + w_hat2 + w_hat3 + w_hat4 + w_hat5;
@@ -21,8 +30,8 @@ w = [w1 w2 w3 w4 w5];
 
 c = cellfun(@(a) a(1, 4), parametersq);
 e = cellfun(@(a) a(1, 2), parametersq);
-c_agg = w * c;
-e_agg = w * e;
+c_agg = w' * c;  % Center of gravity of resulting DF
+e_agg = w' * e;
 
 result = c_agg;
 end
