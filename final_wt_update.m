9d6b45dcbd548eb9d068939cd450f1087653ca3a
Amogh J <amoghjahagirdargsf@gmail.com>
2021-02-24 09:23:36 +0400

    WIP adaptive

---
+++ final_wt_update.m
@@ -0,0 +1,5 @@
+function result = final_wt_update(epi_t, epi_i, error_f, lambda, n_s, d_x, c, w, w_cap, k1, k2)
+    num = 2*lambda*n_s*error_f*d_x*(1 - d_x)*(2*c*w_cap + c*k2 + k1);
+    dtr = epi_i*(w + k2)^2;
+    result = epi_t - num/dtr;
+end
